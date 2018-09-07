2598d51a39ea8ac59794ed3b80f34e914e4ffeae
Ryan Cao <goldenviolin7@gmail.com>
2018-09-07 12:15:51 -0500

    Lesson 2 completed

--- Perfetto/ViewController.swift
+++ Perfetto/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // You can hard-code in lists, just like follows
-    var cardChoices = ["👻", "🧙‍♂️", "👻", "🎃", "🧙‍♂️", "🎃"]
+    // Here's our model! Classes get a free init() with no args as long as all of its args are initialized.
+    lazy var game: Perfetto = Perfetto(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
     
     // IBOutlet is a property (instance field); IBAction is a function
     @IBOutlet weak var flipCountLabel: UILabel!
@@ -35,27 +35,49 @@ class ViewController: UIViewController {
         
         // Unwrapping optionals the safe way - the unsafe way is to simply use !
         if let cardIndex = cardButtons.index(of: sender) {
-            flipCard(withEmoji: cardChoices[cardIndex], on: sender)
+            game.chooseCard(at: cardIndex)
+            updateViewFromModel()
         } else {
             print("Card was not in CardButtons")
         }
-        
-        
-//        // Some crazy optional unwrapping going on here... lol
-//        flipCard(withEmoji: (sender.titleLabel?.text!)!, on: sender)
     }
     
-    // withEmoji is the external name; emoji is the internal name; String is the type.
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        // Equivalent of printf - \(paramName)
-        print("flipCard(withEmoji: \(emoji))")
-        if (button.currentTitle == emoji) {
-            button.setTitle("", for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    // Looks at the model and sees which View elements need to be changed.
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+            }
         }
+    }
+    
+    // Creates mapping from int ID to emoji
+    var emoji = [Int : String]()
+    
+    // You can hard-code in lists, just like follows
+    var emojiChoices = ["👻", "🧙‍♂️", "🎃", "🍎", "🍬", "🍭", "🦇", "🙀", "😱", "😈"]
+    
+    func emoji(for card: Card) -> String {
+        
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            // Takes a random emoji from emojiChoices and puts it into the dictionary at the current key.
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        
+//        if let chosenEmoji = emoji[card.identifier] {
+//            return chosenEmoji
+//        } else {
+//            return "?"
+//        }
+        // Equivalently,
+        return emoji[card.identifier] ?? "?"
     }
 }
 
