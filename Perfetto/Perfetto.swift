f92c7fec4716b01b016dd62696a6f3be35d68d01
Ryan Cao <goldenviolin7@gmail.com>
2018-09-11 13:27:22 -0500

    Changed over to real app

--- Perfetto/Perfetto.swift
+++ Perfetto/Perfetto.swift
@@ -10,41 +10,6 @@ import Foundation
 
 class Perfetto {
     
-    // Public API
-    var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
-    
-    func chooseCard(at index: Int) {
-        if !cards[index].isMatched {
-            
-            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-                if cards[index].identifier == cards[matchIndex].identifier {
-                    cards[index].isMatched = true
-                    cards[matchIndex].isMatched = true
-                }
-                cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
-            } else {
-                // Either no cards or two cards are face up
-                // Either way, turn all cards on the board face-down.
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = index
-            }
-            
-        }
-    }
-    
-    init(numberOfPairsOfCards: Int) {
-        // Swift's for-loop and equivalent to Python's range()
-        for _ in 0..<numberOfPairsOfCards {
-            let card = Card()
-            cards += [card, card] // This creates two copies and adds them to the card array!
-        }
-        // TODO: Shuffle the cards!
-    }
     
 }
--- Perfetto/ViewController.swift
+++ Perfetto/ViewController.swift
@@ -7,77 +7,31 @@
 //
 
 import UIKit
+import CoreGraphics.CGContext
 
 class ViewController: UIViewController {
 
-    // Here's our model! Classes get a free init() with no args as long as all of its args are initialized.
-    lazy var game: Perfetto = Perfetto(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    @IBOutlet weak var profileButton: UIButton!
     
-    // IBOutlet is a property (instance field); IBAction is a function
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet weak var referralButton: UIButton!
     
-    // Swift will automagically infer the types of declared variables.
-    // OMG - this didSet is like an onSetListener - it'll run the following
-    // code every single time flipCount's value changes!
-    var flipCount = 0 {
-        didSet {
-            // Change the flipCount text displayed
-            flipCountLabel.text = "Flips: \(flipCount)"
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "heart_pizza_background.jpg")!)
     }
     
-    // Swift array - must know which type it is!
-    @IBOutlet var cardButtons: [UIButton]!
-    
-    @IBAction func touchCard(_ sender: UIButton) {
-        // Increment number of times we've flipped cards
-        flipCount += 1
-        
-        // Unwrapping optionals the safe way - the unsafe way is to simply use !
-        if let cardIndex = cardButtons.index(of: sender) {
-            game.chooseCard(at: cardIndex)
-            updateViewFromModel()
-        } else {
-            print("Card was not in CardButtons")
+}
+
+// Adds an extension to the base integer class.
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else { // if self == 0
+            return 0
         }
-    }
-    
-    // Looks at the model and sees which View elements need to be changed.
-    func updateViewFromModel() {
-        for index in cardButtons.indices {
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            if card.isFaceUp {
-                button.setTitle(emoji(for: card), for: UIControlState.normal)
-                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            } else {
-                button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
-            }
-        }
-    }
-    
-    // Creates mapping from int ID to emoji
-    var emoji = [Int : String]()
-    
-    // You can hard-code in lists, just like follows
-    var emojiChoices = ["👻", "🧙‍♂️", "🎃", "🍎", "🍬", "🍭", "🦇", "🙀", "😱", "😈"]
-    
-    func emoji(for card: Card) -> String {
-        
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            // Takes a random emoji from emojiChoices and puts it into the dictionary at the current key.
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
-        }
-        
-//        if let chosenEmoji = emoji[card.identifier] {
-//            return chosenEmoji
-//        } else {
-//            return "?"
-//        }
-        // Equivalently,
-        return emoji[card.identifier] ?? "?"
     }
 }
 
